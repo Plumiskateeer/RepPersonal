211e55426eda402945368a83b779622eaf11e102
amjyebenes <angelote2229914@gmail.com>
2021-10-15 14:47:27 +0200

    Ej CP4

--- EntornosDesarrolloDAW/src/Ejercicio5_CP4.java
+++ EntornosDesarrolloDAW/src/Ejercicio5_CP4.java
@@ -1,7 +1,18 @@
+import javax.swing.*;
+
 public class Ejercicio5_CP4 {
     public static void main(String[]args){
-        int i = 0, num, suma = 0;
+        int i = 0, num = 1, suma = 0;
 
+        while(num!=0){
+            num = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero para hacer la media: "));
+            if(num>0){
+                suma+=num;
+                i++;
+            }
+        }
+        if(i>0) System.out.println("La media es: " + suma/i);
+        else System.out.println("No se han introducido numeros.");
         
     }
 }
